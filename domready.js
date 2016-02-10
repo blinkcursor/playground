6572a5e7c37c4b137cb85d74e72d4e94cc34ff43
terraling <nigel.m.anderson@gmail.com>
2016-02-10 19:56:18 +0100

    add cloned image to DOM

--- domready.js
+++ domready.js
@@ -1,10 +1,12 @@
 ;(function(){
 	btn = document.querySelector('button');
 	btn.addEventListener('click', cloneImg, false);
+	clone = document.querySelector('.clone-wrapper');
 
 	function cloneImg(e){
 		newImg = document.querySelector('img').cloneNode(true);
 		newImg.sizes = "100vw";
 		console.log(newImg);
+		clone.appendChild(newImg);
 	}
 })()
