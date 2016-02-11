017a72b27ec193b81cf3db5dab8a3a5df6c79045
terraling <nigel.m.anderson@gmail.com>
2016-02-11 15:33:34 +0100

    log some tests

--- domready.js
+++ domready.js
@@ -4,8 +4,10 @@
 	clone = document.querySelector('.clone-wrapper');
 
 	function cloneImg(e){
-		newImg = document.querySelector('img').cloneNode(true);
+		newImg = document.querySelector('.wrapper33 > img').cloneNode(true);
+		console.log(newImg.getAttribute('sizes'));
 		newImg.setAttribute('sizes', '100vw');
 		clone.appendChild(newImg);
+		console.log(newImg.getAttribute('sizes'));
 	}
 })()
